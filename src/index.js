7646dc9cd2d58a092a96f120e90e71575fbdd56e
cody <cevek@ya.ru>
2015-09-19 19:42:31 +0300

    work version with localstorage

--- src/index.js
+++ src/index.js
@@ -19,7 +19,9 @@ class Word {
     }
 
     setKey(keyMap) {
-        let key = this.text.toLocaleLowerCase().replace(/[^\w\d]+/, '');
+        let key = this.text.toLocaleLowerCase().replace(/[^\w\d]+/g, '');
+        //console.log(this.text, key);
+
         while (keyMap[key]) {
             key += '*';
         }
@@ -32,11 +34,16 @@ class Words {
     constructor() {
         this.sync = null;
         this.words = null;
+        this.postId = 'alisa';
+        this.postData = alisaData;
+        this.currentLine = 0;
         this.render();
+        this.fill();
+        this.setNextSentence();
     }
 
     doit(userText) {
-        let originText = "Alisa buys a book with her money";
+        let originText = this.postData.data[this.currentLine][0];
 
         this.words = this.parseWords(userText);
         let originWords = this.parseWords(originText);
@@ -45,16 +52,56 @@ class Words {
 
         this.modify();
 
-        this.merge();
         this.fixOrder();
+        this.merge();
 
         this.renderLine();
+
+        this.currentLine++;
+        this.setNextSentence();
+
         console.log(this.words, this.sync);
+    }
+
+    getData() {
+        try {
+            var item = localStorage[this.postId];
+            if (item) {
+                var dt = JSON.parse(item);
+            }
+            else {
+                dt = {lines: []};
+            }
+        }
+        catch (e) {
+            console.error(e);
+            dt = {lines: []};
+        }
+        return dt;
+    }
+
+    fill() {
+        var data = this.getData();
+        for (var i = 0; i < data.lines.length; i++) {
+            var line = data.lines[i];
+            this.doit(line);
+        }
+    }
+
+    setNextSentence() {
+        this.translate.textContent = this.postData.data[this.currentLine][1];
+    }
+
+    saveLine() {
+        var data = this.getData();
+        data.lines.push(this.text.value);
+        localStorage[this.postId] = JSON.stringify(data);
+        this.text.value = '';
     }
 
     parseWords(str) {
         let keyMap = {};
-        let wordChunks = this.prepareStr(str).split(/\b(?=\w)/);
+        let wordChunks = this.prepareStr(str).split(/ /);
         let words = [];
         for (let i = 0; i < wordChunks.length; i++) {
             words.push(new Word(wordChunks[i], keyMap));
@@ -64,18 +111,19 @@ class Words {
     }
 
     prepareStr(str) {
-        str = str.replace(/\b(are|did|do|does|can|could|had|have|has|is|might|may|must|was|were|would) ?not\b/i, "$1n’t");
-        str = str.replace(/\b(I|he|she|they|we|you) (had)\b/i, "$1’d");
-        str = str.replace(/\b(I|they|we|you) have\b/i, "$1’ve");
-        str = str.replace(/\b(he|she|here|that|there|what) is\b/i, "$1’s");
-        str = str.replace(/\bI\b/i, "I");
-        str = str.replace(/\b(I) am\b/i, "$1’m");
-        str = str.replace(/\b(W)ill not\b/i, "$1on’t");
-        str = str.replace(/\b(let) us\b/i, "$1’s");
-        str = str.replace(/\b(I|you|he|she|it|we|they|that) will\b/i, "$1’ll");
-        str = str.replace(/'/i, "’");
-        str = str.replace(/\s+/, " ");
-        str = str[0].toUpperCase() + str.slice(1);
+        str = str.replace(/(-|–|—) +/ig, "$1\u00A0");
+        str = str.replace(/\b(are|did|do|does|can|could|had|have|has|is|might|may|must|was|were|would) ?not\b/ig, "$1n’t");
+        str = str.replace(/\b(I|he|she|they|we|you) (had)\b/ig, "$1’d");
+        str = str.replace(/\b(I|they|we|you) have\b/ig, "$1’ve");
+        str = str.replace(/\b(he|she|here|that|there|what) is\b/ig, "$1’s");
+        str = str.replace(/\bI\b/ig, "I");
+        str = str.replace(/\b(I) am\b/ig, "$1’m");
+        str = str.replace(/\b(W)ill not\b/ig, "$1on’t");
+        str = str.replace(/\b(let) us\b/ig, "$1’s");
+        str = str.replace(/\b(I|you|he|she|it|we|they|that) will\b/ig, "$1’ll");
+        str = str.replace(/'/ig, "’");
+        str = str.replace(/\s+/g, " ");
+        //str = str[0].toUpperCase() + str.slice(1);
         return str;
     }
 
@@ -105,25 +153,53 @@ class Words {
                     pos = this.words.findIndex(word => word.key == block.next.key);
                 }
                 var newWord = new Word(word.text, this.words.keyMap);
+                newWord.movedFrom = word;
                 newWord.type = TOKEN.movedTo;
                 newWord.key = word.key;
                 this.words.splice(pos, 0, newWord);
                 word.key = null;
                 word.type = TOKEN.movedFrom;
+                word.movedTo = newWord;
             }
         }
     }
 
     merge() {
-        for (var i = 0; i < this.words.length - 1; i++) {
-            var word = this.words[i];
-            var nextWord = this.words[i + 1];
-            if (word.type == nextWord.type/* && word.type !== TOKEN.movedFrom && word.type !== TOKEN.movedTo*/) {
-                this.words.splice(i, 1);
-                nextWord.text = word.text + nextWord.text;
-                i--;
+        var lastWord = this.words[0];
+        var newWords = [];
+        newWords.push(lastWord);
+        for (var i = 1; i < this.words.length; i++) {
+            var prevWord = this.words[i - 1];
+            var thisWord = this.words[i];
+            var merge = false;
+
+            if (prevWord.type == TOKEN.movedFrom && thisWord.type == TOKEN.movedFrom) {
+                var to1Pos = this.words.indexOf(prevWord.movedTo);
+                var to2Pos = this.words.indexOf(thisWord.movedTo);
+                if (to1Pos == to2Pos - 1) {
+                    merge = true;
+                }
+            }
+            else if (prevWord.type == TOKEN.movedTo && thisWord.type == TOKEN.movedTo) {
+                var from1Pos = this.words.indexOf(prevWord.movedFrom);
+                var from2Pos = this.words.indexOf(thisWord.movedFrom);
+                if (from1Pos == from2Pos - 1) {
+                    merge = true;
+                }
+            }
+            else if (prevWord.type == thisWord.type) {
+                merge = true;
+            }
+
+            if (merge) {
+                lastWord.text = lastWord.text + ' ' + thisWord.text;
+            }
+            else {
+                newWords.push(thisWord);
+                lastWord = thisWord;
             }
         }
+        this.words = newWords;
     }
 
     fixOrder() {
@@ -149,6 +225,7 @@ class Words {
 
     onSubmit() {
         this.doit(this.text.value);
+        this.saveLine();
         return false;
     }
 
@@ -156,6 +233,9 @@ class Words {
         this.app = document.createElement('div');
         this.app.classList.add('app');
         document.body.appendChild(this.app);
+
+        this.svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        this.app.appendChild(this.svg);
 
         this.items = document.createElement('div');
         this.items.classList.add('items');
@@ -166,6 +246,10 @@ class Words {
         this.form.onsubmit = ()=>this.onSubmit();
         this.app.appendChild(this.form);
 
+        this.translate = document.createElement('div');
+        this.translate.classList.add('translate');
+        this.form.appendChild(this.translate);
+
         this.text = document.createElement('input');
         this.text.classList.add('text');
         this.text.setAttribute('type', 'text');
@@ -173,12 +257,56 @@ class Words {
         this.form.appendChild(this.text);
     }
 
+    generateArrowPath(y, from, to) {
+        var height = 20;
+        var arrowSize = 5;
+        var fromShift = 0;
+        var toShift = 0;
+        if (from < to) {
+            var right = to;
+            var left = from;
+            toShift = arrowSize;
+        }
+        else {
+            right = from;
+            left = to;
+            fromShift = arrowSize;
+        }
+        var top = y - height;
+        return [
+            `M${to - arrowSize},${y - arrowSize} h${arrowSize * 2} l-${arrowSize},${arrowSize} Z`,
+            `M${left},${y - fromShift} C${left},${top} ${right},${top} ${right},${y - toShift}`
+        ];
+    }
+
+    renderArrows(parent, arrows) {
+        var xmlns = "http://www.w3.org/2000/svg";
+        for (var i = 0; i < arrows.length; i++) {
+            var arrow = arrows[i];
+            var fromBounds = arrow.from.dom.getBoundingClientRect();
+            var toBounds = arrow.to.dom.getBoundingClientRect();
+            var paths = this.generateArrowPath(fromBounds.top, fromBounds.left + fromBounds.width / 2 | 0, toBounds.left + toBounds.width / 2 | 0);
+
+            var pathNode = document.createElementNS(xmlns, 'path');
+            pathNode.setAttribute('d', paths[1]);
+            pathNode.setAttribute('class', 'line');
+            this.svg.appendChild(pathNode);
+
+            var arrowNode = document.createElementNS(xmlns, 'path');
+            arrowNode.setAttribute('d', paths[0]);
+            arrowNode.setAttribute('class', 'arrow');
+            this.svg.appendChild(arrowNode);
+        }
+
+    }
+
+
     renderLine() {
         var node = document.createElement('div');
         node.classList.add('line');
         this.items.appendChild(node);
 
-
+        var arrows = [];
         for (let i = 0; i < this.words.length; i++) {
             let word = this.words[i];
             let span = document.createElement('span');
@@ -187,14 +315,18 @@ class Words {
                 span.classList.add(TOKEN.token);
                 span.classList.add(word.type);
             }
+            if (word.type == 'moved-to') {
+                arrows.push({from: word.movedFrom, to: word});
+            }
             word.dom = span;
             span.textContent = word.text.trim();
             node.appendChild(document.createTextNode(' '));
         }
+        this.renderArrows(node, arrows);
     }
 }
 
 
 var w = new Words();
-w.doit('for her money alisa buys the book');
+//w.doit('with her money alisa the book buys');
 
---
+++ src/posts/alisa/1.js
@@ -0,0 +1,496 @@
+var alisaData = {
+  "data": [
+    [
+      "Alissa is reading.",
+      "Алиса читает."
+    ],
+    [
+      "Her father calls to her.",
+      "Ее отец зовет ее."
+    ],
+    [
+      "‘Alissa! Alissa!’",
+      "- Алиса! Алиса!"
+    ],
+    [
+      "Alissa runs to the door.",
+      "Алиса бежит к двери."
+    ],
+    [
+      "There is a car outside the house.",
+      "Возле дома стоит автомобиль."
+    ],
+    [
+      "Her father is talking to a fat man.",
+      "Отец разговаривает с толстяком."
+    ],
+    [
+      "‘This is Alissa.",
+      "- Это Алиса."
+    ],
+    [
+      "She reads all day,’ her father says.",
+      "Она читает весь день, - говорит ее отец."
+    ],
+    [
+      "The two men laugh.",
+      "Двое мужчин смеются."
+    ],
+    [
+      "‘Alissa,’ her father says.",
+      "- Алиса, - говорит ее отец."
+    ],
+    [
+      "‘My friend has work for you in the city.",
+      "- У моего друга есть работа для тебя в городе."
+    ],
+    [
+      "There isn’t any work here in the village.",
+      "Нет никакой работы здесь, в деревне."
+    ],
+    [
+      "You must go with him.’",
+      "Ты должна отправиться с ним."
+    ],
+    [
+      "The fat man smiles at Alissa.",
+      "Толстяк улыбается Алисе."
+    ],
+    [
+      "‘How old are you?’ he asks.",
+      "- Сколько тебе лет? - спрашивает он."
+    ],
+    [
+      "‘I’m twelve,’ she says.",
+      "- Мне двенадцать, - говорит она."
+    ],
+    [
+      "The fat man laughs again.",
+      "Толстяк снова смеется."
+    ],
+    [
+      "Alissa doesn’t like him.",
+      "Алисе он не нравится."
+    ],
+    [
+      "She doesn’t want to go with this man.",
+      "Она не хочет ехать с этим человеком."
+    ],
+    [
+      "She wants to go to school in the village.",
+      "Она хочет ходить в школу в деревне."
+    ],
+    [
+      "She likes school.",
+      "Ей нравится школа."
+    ],
+    [
+      "She likes reading.",
+      "Ей нравится читать."
+    ],
+    [
+      "‘Your mother is packing your things.",
+      "- Твоя мать укладывает твои вещи."
+    ],
+    [
+      "You must go to the city,’ her father says.",
+      "Ты должна отправиться в город, - говорит ее отец."
+    ],
+    [
+      "The fat man gives some money to Alissa’s father.",
+      "Толстяк дает денег отцу Алисы."
+    ],
+    [
+      "Alissa’s father is pleased and happy.",
+      "Отец Алисы довольный и счастливый."
+    ],
+    [
+      "Alissa is angry and afraid.",
+      "Алиса сердитая и боится."
+    ],
+    [
+      "Alissa does not want to go with this man.",
+      "Алиса не хочет ехать с этим человеком."
+    ],
+    [
+      "But she must obey her father.",
+      "Но она должна подчиняться отцу."
+    ],
+    [
+      "Alissa and the fat man arrive in the city.",
+      "Алиса и толстяк прибывают в город."
+    ],
+    [
+      "They drive to a house.",
+      "Они подъезжают к какому-то дому."
+    ],
+    [
+      "A thin man comes to the door.",
+      "Худой мужчина выходит из двери."
+    ],
+    [
+      "The two men talk.",
+      "Двое мужчин разговаривают."
+    ],
+    [
+      "‘Here is your room,’ the thin man says to Alissa.",
+      "- Вот твоя комната, - худой мужчина говорит Алисе."
+    ],
+    [
+      "He points to a door under the steps.",
+      "Он указывает на дверь под ступеньками."
+    ],
+    [
+      "Alissa goes into the room.",
+      "Алиса идет в комнату."
+    ],
+    [
+      "The room is small and dark.",
+      "Комната маленькая и темная."
+    ],
+    [
+      "It is her new home.",
+      "Это ее новый дом."
+    ],
+    [
+      "The next morning, the thin man takes Alissa into the house.",
+      "На следующее утро худой человек берет Алису в дом."
+    ],
+    [
+      "‘This is Alissa,’ the man says to his wife.",
+      "- Это Алиса, - говорит мужчина своей жене."
+    ],
+    [
+      "‘She likes reading.’",
+      "- Ей нравится чтение."
+    ],
+    [
+      "They laugh.",
+      "Они смеются."
+    ],
+    [
+      "Suddenly the woman shouts at Alissa.",
+      "Вдруг женщина кричит на Алису."
+    ],
+    [
+      "‘You aren’t going to read here,’ she shouts.",
+      "- Ты не будешь читать здесь, - она кричит."
+    ],
+    [
+      "‘You’re going to cook and clean and wash.’",
+      "- Ты будешь готовить, убирать и мыть."
+    ],
+    [
+      "Alissa works fifteen hours a day.",
+      "Алиса работает по пятнадцать часов в день."
+    ],
+    [
+      "The woman shouts at her every day.",
+      "Женщина кричит на нее каждый день."
+    ],
+    [
+      "Alissa is very unhappy.",
+      "Алиса очень несчастна."
+    ],
+    [
+      "She cries every night.",
+      "Она плачет каждую ночь."
+    ],
+    [
+      "One day, the thin man says to Alissa,",
+      "Однажды, худой мужчина говорит Алисе:"
+    ],
+    [
+      "‘Pack your things. You must go.",
+      "- Собирай свои вещи. Ты должна уйти."
+    ],
+    [
+      "My wife doesn’t like you.’",
+      "Ты не нравишься моей жене."
+    ],
+    [
+      "The thin man takes her to a clothes shop in the city.",
+      "Худой мужчина забирает ее в магазин одежды в городе."
+    ],
+    [
+      "The shop owner is a large woman.",
+      "Владельцем магазина является крупная женщина."
+    ],
+    [
+      "She gives the man some money.",
+      "Она дает мужчине деньги."
+    ],
+    [
+      "He goes away.",
+      "Он уходит."
+    ],
+    [
+      "He doesn’t say goodbye to Alissa.",
+      "Он не говорит до свидание Алисе."
+    ],
+    [
+      "Alissa works with five other girls.",
+      "Алиса работает с пятью другими девушками."
+    ],
+    [
+      "They work in a small, dark room.",
+      "Они работают в маленькой, темной комнате."
+    ],
+    [
+      "The girls work all day.",
+      "Девушки работают весь день."
+    ],
+    [
+      "They make clothes.",
+      "Они делают одежду."
+    ],
+    [
+      "They work twelve hours a day.",
+      "Они работают по двенадцать часов в день."
+    ],
+    [
+      "At midday, they eat lunch.",
+      "В полдень они едят ланч."
+    ],
+    [
+      "After lunch, they rest for ten minutes.",
+      "После ланча они отдыхают в течение десяти минут."
+    ],
+    [
+      "At night, they sleep on the floor.",
+      "Ночью они спят на полу."
+    ],
+    [
+      "Each month, the shop owner gives the girls a little money.",
+      "Каждый месяц владелица магазина дает девушкам немного денег."
+    ],
+    [
+      "Alissa buys a book with her money.",
+      "Алиса покупает книгу на свои деньги."
+    ],
+    [
+      "She reads the book after lunch.",
+      "Она читает книгу после обеда."
+    ],
+    [
+      "The shop owner is surprised.",
+      "Владелец магазина удивлена."
+    ],
+    [
+      "The other girls can’t read.",
+      "Другие девушки не умеют читать."
+    ],
+    [
+      "‘Can you write? Can you count?’ the shop owner asks.",
+      "- Ты можешь писать? Ты можешь считать? - спрашивает владелица магазина."
+    ],
+    [
+      "‘Yes, I can,’ Alissa says.",
+      "- Да, я умею, - говорит Алиса."
+    ],
+    [
+      "‘Come,’ the shop owner says.",
+      "- Пойдем, - говорит владелица магазина."
+    ],
+    [
+      "‘You are going to work in the shop.’",
+      "- Ты будешь работать в магазине."
+    ],
+    [
+      "Alissa likes working in the shop.",
+      "Алисе нравится работать в магазине."
+    ],
+    [
+      "She serves the customers.",
+      "Она обслуживает покупателей."
+    ],
+    [
+      "The customers are rich ladies.",
+      "Покупатели - богатые дамы."
+    ],
+    [
+      "They buy expensive dresses.",
+      "Они покупают дорогие платья."
+    ],
+    [
+      "One of the customers is a tall and pretty lady.",
+      "Одним из покупателей является высокая и красивая дама."
+    ],
+    [
+      "She always smiles at Alissa.",
+      "Она всегда улыбается Алисе."
+    ],
+    [
+      "She gives Alissa small presents.",
+      "Она дает Алисе небольшие подарки."
+    ],
+    [
+      "One day, the tall lady leaves her purse in the shop.",
+      "Однажды высокая дама оставляет свой кошелек в магазине."
+    ],
+    [
+      "Alissa runs out into the street.",
+      "Алиса выбегает на улицу."
+    ],
+    [
+      "She runs after the lady.",
+      "Она бежит за дамой."
+    ],
+    [
+      "‘Here is your purse,’ Alissa says.",
+      "- Вот ваш кошелек, - говорит Алиса."
+    ],
+    [
+      "The lady smiles.",
+      "Дама улыбается."
+    ],
+    [
+      "She takes some money from the purse.",
+      "Она берет деньги из своего кошелька."
+    ],
+    [
+      "‘Thank you,’ she says to Alissa.",
+      "- Спасибо тебе, - говорит она Алисе."
+    ],
+    [
+      "‘You are an honest girl. Take this money.’",
+      "- Ты честная девушка. Возьми эти деньги."
+    ],
+    [
+      "‘No, no,’ says Alissa.",
+      "- Нет, нет, - говорит Алиса."
+    ],
+    [
+      "‘I don’t want your money.’",
+      "- Я не хочу ваши деньги."
+    ],
+    [
+      "She runs back to the shop.",
+      "Она бежит обратно в магазин."
+    ],
+    [
+      "The shop owner shouts at her.",
+      "Владелица магазина кричит на нее."
+    ],
+    [
+      "‘Don’t leave the shop again!’ she shouts.",
+      "- Не оставляй магазин снова! - кричит она."
+    ],
+    [
+      "‘I pay you a lot of money.",
+      "- Я плачу тебе много денег."
+    ],
+    [
+      "I pay you to work.",
+      "Я плачу тебе, чтобы ты работала."
+    ],
+    [
+      "I don’t pay you to run out into the street.’",
+      "Я не плачу тебе, чтобы ты выбегала на улицу."
+    ],
+    [
+      "Alissa is angry.",
+      "Алиса сердится."
+    ],
+    [
+      "‘You don’t pay me a lot of money,’ she shouts.",
+      "- Вы не платите мне много денег, - она кричит."
+    ],
+    [
+      "‘I’m a slave here.’",
+      "- Я рабыня здесь."
+    ],
+    [
+      "‘You're an ungrateful girl,’ the large woman says.",
+      "- Ты неблагодарная девушка, - говорит крупная женщина."
+    ],
+    [
+      "‘You have a bed and food and money.",
+      "- У тебя есть и кровать, и питание, и деньги."
+    ],
+    [
+      "Do you want more?’",
+      "Хочешь больше?"
+    ],
+    [
+      "‘Yes, I do,’ Alissa says.",
+      "- Да, - говорит Алиса."
+    ],
+    [
+      "She is crying now.",
+      "Она плачет теперь."
+    ],
+    [
+      "‘Wait,’ a quiet voice says.",
+      "- Подожди, - говорит тихий голос."
+    ],
+    [
+      "The tall lady is standing at the door.",
+      "Высокая дама стоит в дверях."
+    ],
+    [
+      "‘Alissa isn’t ungrateful,’ the tall lady says.",
+      "- Алиса не неблагодарная, - говорит высокая дама."
+    ],
+    [
+      "‘She is an honest girl.’",
+      "- Она честная девушка."
+    ],
+    [
+      "The tall lady speaks to Alissa.",
+      "Высокая дама говорит Алисе."
+    ],
+    [
+      "‘Aren’t you happy here?’ she asks.",
+      "- Ты не счастлива здесь? - спрашивает она."
+    ],
+    [
+      "‘What do you want?’",
+      "- Что ты хочешь?"
+    ],
+    [
+      "Alissa says, ‘I want to go to school.’",
+      "Алиса говорит:"
+    ],
+    [
+      "The tall lady turns to the shop owner.",
+      "Высокая дама поворачивается к владелице магазина."
+    ],
+    [
+      "‘Alissa will live in my house,’ she says.",
+      "- Алиса будет жить в моем доме, - говорит она."
+    ],
+    [
+      "‘She won’t work. She will go to school.’",
+      "- Она не будет работать. Она будет ходить в школу."
+    ],
+    [
+      "‘You must pay me,’ the shop owner says.",
+      "- Вы должны заплатить мне, - говорит владелица магазина."
+    ],
+    [
+      "‘No,’ the tall lady says, ‘Alissa isn’t a slave.’",
+      "- Нет, - говорит высокая дама, - Алиса не раб."
+    ],
+    [
+      "‘Pack your things, Alissa,’ she says.",
+      "- Собирай свои вещи, Алиса, - говорит она."
+    ],
+    [
+      "‘We will go home now.’",
+      "- Мы пойдем домой сейчас."
+    ],
+    [
+      "Alissa goes with the tall lady.",
+      "Алиса уходит с высокой дамой."
+    ],
+    [
+      "She is going to a new home.",
+      "Она направляется в новый дом."
+    ],
+    [
+      "She is going to be happy.",
+      "Она будет счастлива."
+    ]
+  ]
+}
